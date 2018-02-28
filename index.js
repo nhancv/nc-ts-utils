237dfc4fad8e58c9ba7a54c9f4e87f9e231a7b8c
Nhan Cao <nhancv92@gmail.com>
2018-02-28 09:46:20 +0700

    socketio

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
+var express = require('express')
+var app = express()
+var path = require('path')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var port = process.env.PORT || 3000
 
+// Routing
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+  socket.on('foo', function(msg){
+    console.log('Foo: ', msg)
+    socket.emit('event', msg)
+  })
+})
+
+http.listen(port, function () {
+  console.log('Server listening at port %d', port)
+})
