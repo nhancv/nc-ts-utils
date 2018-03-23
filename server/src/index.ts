6ba8a4b3761965bc91ea49576ecb67b361c84497
Nhan Cao <nhancv92@gmail.com>
2018-03-23 10:11:38 +0700

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,19 @@
 const express = require('express')
-const app = express()
 const path = require('path')
-
+const app = express()
 const log = console.log
 var port = process.env.PORT || 4000
 
-// Routing
-var frontendDir = path.join(
-  path.dirname(path.dirname(__dirname)),
-  'frontend'
-)
-
-app.use(express.static(path.join(frontendDir, 'build')))
-app.get('/', function(req, res) {
+// For fontend route
+var frontendDir = path.join(path.dirname(path.dirname(__dirname)), 'frontend')
+app.use('/home', express.static(path.join(frontendDir, 'build')))
+app.get('/home', function(req, res) {
   res.sendFile(path.join(frontendDir, 'build', 'index.html'))
 })
+app.get('/', function(req, res) {
+  res.redirect('/home');
+})
+
 app.listen(port, function() {
   log('Server listening at port %d', port)
 })
