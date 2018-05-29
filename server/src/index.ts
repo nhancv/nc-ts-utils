8f014f20239fa1583627929c16a60196c3319dc2
Nhan Cao <nhancv92@gmail.com>
2018-05-29 09:07:13 +0700

    update server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
 const app = express()
 const log = console.log
 var port = process.env.PORT || 4000
 
+// Body parser: https://github.com/expressjs/body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 // CORS on ExpressJS: https://github.com/expressjs/cors
 app.use(cors())
+// Cookie parser: https://github.com/expressjs/cookie-parser
+app.use(cookieParser())
 
 // For fontend route
 var frontendDir = path.join(path.dirname(path.dirname(__dirname)), 'frontend')
