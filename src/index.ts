b64b47a28438867fb797e4b7c4e47b664899a792
Nhan Cao <nhancv92@gmail.com>
2018-10-10 17:55:16 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
 
 const config = require('./env.js').get(process.env.NODE_ENV);
 const app = express();
@@ -15,11 +16,27 @@ app.use(bodyParser.json());
 app.use(cors());
 // Cookie parser: https://github.com/expressjs/cookie-parser
 app.use(cookieParser());
+// Use gzip compression
+app.use(compression());
 
-app.get('/', function(req, res) {
-  res.send('<center><h1>Hello cu</h1></center>');
+/**
+ * API calls, use Postman for testing
+ * This block should declare before default route
+ */
+app.get('/api/about', function (req, res) {
+  res.json({'about': 'https://nhancv.github.io'});
 });
 
+/**
+ * Default routing
+ */
+app.get('*', function (req, res) {
+  res.redirect('/api/about')
+});
+
+/**
+ * Start listen
+ */
 app.listen(port, function() {
   log('Server listening at port %d', port);
 });
