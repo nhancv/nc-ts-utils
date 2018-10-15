eaf8aed6638d2a4d78aab92d5a2eabc3e7d039b0
Nhan Cao <nhancv92@gmail.com>
2018-10-15 16:26:32 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -25,14 +25,14 @@
  */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
     if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
     result["default"] = mod;
     return result;
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
